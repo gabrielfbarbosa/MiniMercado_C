aa0dd3d9f7834c97f7db0b13c9b933735b65558e
Gabriel <gfelipebarbosa@yahoo.com>
2023-10-17 16:52:19 -0300

    start project

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <limits.h>
+
+
+typedef struct
+{
+    int item;
+    struct item *prox;
+}item;
+
+
+int main(void){
+
+
+    printf("\n\n");
+    return 0;
+}
